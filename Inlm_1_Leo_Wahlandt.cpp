a1d065b6293b5dcc582ea1ea5ef071170a829de1
Leo Wahlandt <leo.wahlandtcs@gmail.com>
2021-09-30 14:09:13 +0200

    Addded some comments and fixed some issues

--- Inlm_1_Leo_Wahlandt.cpp
+++ Inlm_1_Leo_Wahlandt.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <ctime>
+#include <regex>
 
 using namespace std;
 
@@ -10,7 +11,6 @@ int main()
 {
 	int balance;
 	int bet{};
-	int bet;
 
 	//
 	bool wish2Continue = true;
@@ -20,26 +20,31 @@ int main()
 	int humanGameScore{};
 	int computerGameScore{};
 
+	int totalWinnings = 0;
+
+	bool invalidInput = false;
+	bool closeGame = true;
+
 	//Betting loop is how much you want to insert in a loop. If you dont have enough money and dont want to insert more it quits.
-	bool Betting = true;
+	bool betting = true;
 
 	srand(time(NULL));
 
 	cout << "Welcome To Valen's Dice game!" << endl;
 
-	while (Betting) {
+	while (betting) {
 		cout << "Type in your wishing Balance. (Max 5000): "; cin >> balance;
 
+		//This wish2Continue is set to true if the player want to play again but dont have enough money and have to insert more.
 		wish2Continue = true;
 
 
-		if (balance > 5001 || balance < 0) {
+
+		if (balance < 0 || balance > 5001) {
 			cout << "Invalid amount";
 			wish2Continue = false;
 
 		}
-
-		char removeWhile;
 
 		while (wish2Continue) {
 			//The user can only bet 100, 300 or 500 kr. Therefore there's 3 options only.
@@ -52,24 +57,26 @@ int main()
 			if (bet == 1 && balance >= 100) {
 				bet = 100;
 				balance -= bet;
-				cout << "You bet " << bet << endl;
+				cout << "You bet " << bet << " kr " << endl;
 			}
 			else if (bet == 2 && balance >= 300) {
 				bet = 300;
 				balance -= bet;
-				cout << "You bet " << bet << endl;
+				cout << "You bet " << bet << " kr " << endl;
 			}
 			else if (bet == 3 && balance >= 500) {
 				bet = 500;
 				balance -= bet;
-				cout << "You bet " << bet << endl;
+				cout << "You bet " << bet << " kr " << endl;
 			}
 			else {
 				cout << "Invalid option or not enough balance" << endl;
 				bet = 0;
 				diceGame = false;
+				invalidInput = true;
 			}
 
+			//The score is reset before the game start if the player has already played the game
 			int score = 0;
 			int computerScore = 0;
 
@@ -80,46 +87,53 @@ int main()
 				int humanDice1 = 1 + (rand() % 6);
 				int humanDice2 = 1 + (rand() % 6);
 
-				cout << "This is both dices for human: " << endl << humanDice1 << endl << humanDice2 << endl;
+				cout << "--------------You rolls the dices-----------------" << endl;
+
+				cout << "This is both dices for human: " << humanDice1 << " & " << humanDice2 << endl;
 
 				if (humanDice1 > humanDice2) {
 
 					//The first dice is the better one
 					bestHumanDice = humanDice1;
-					cout << "Best dice is number one" << bestHumanDice << endl;
+					cout << "Your best dice is: " << bestHumanDice << endl << endl;
 
 				}
 				else if (humanDice1 < humanDice2) {
 					//The second dice is the better one
 					bestHumanDice = humanDice2;
-					cout << "Best dice is number two" << bestHumanDice << endl;
+					cout << "Your best dice is:  " << bestHumanDice << endl << endl;
 				}
 				else {
 					//Both dices are the same so therefore we send in the first dice
 					bestHumanDice = humanDice1;
-					cout << "Both dices are the same" << bestHumanDice << endl;
+					cout << "Both dices are the same" << bestHumanDice << endl << endl;
 				}
+
+				cout << "--------------Computer rolls the dices-----------------" << endl;
 
 				int computerDice1 = 1 + (rand() % 6);
 				int computerDice2 = 1 + (rand() % 6);
 
-				cout << "This is both dices for Computer: " << endl << computerDice1 << endl << computerDice2 << endl;
+				cout << "The Computer rolled: " << computerDice1 << " & " << computerDice2 << endl;
 
 
 				//All comment is on Human Dice
 				if (computerDice1 > computerDice2) {
 					bestComputerDice = computerDice1;
-					cout << "Best dice is number one " << bestComputerDice << endl;
+					cout << "Best dice is number one " << bestComputerDice << endl << endl;
 				}
 				else if (computerDice2 > computerDice1) {
 					bestComputerDice = computerDice2;
-					cout << "Best dice is number two " << bestComputerDice << endl;
+					cout << "Best dice is number two " << bestComputerDice << endl << endl;
 				}
 				else {
 					bestComputerDice = computerDice1;
-					cout << "Both dices are the same " << bestComputerDice << endl;
+					cout << "Both dices are the same " << bestComputerDice << endl << endl;
 				}
 
+				cout << "--------------The battle-----------------" << endl;
+
+				cout << "The Human dice is: " << bestHumanDice << " Vs Computer dice is: " << bestComputerDice << endl ;
 
 				//Checks who win with their best dices
 				if (bestHumanDice > bestComputerDice) {
@@ -131,19 +145,21 @@ int main()
 				else {
 					computerScore++;
 				}
-				cout << "The score is now for human: " << score << "The computer score: " << computerScore << endl;
+				cout << "The score is now for human: " << score << " The computer score: " << computerScore << endl << endl;
 
 				//If the user win 2 rounds the balance will add 2x the bet. If the user loses the user loses the bet,
 				if (score == 2) {
-					balance += (bet * 2);
+					balance += bet * 2;
+					totalWinnings += bet;
 					cout << "Nice you won! Your new balance is: " << balance << endl;
 					humanGameScore++;
-					diceGame = false;
+					break;
 				}
 				else if (computerScore == 2) {
+					totalWinnings -= bet;
 					cout << "You lost! Your new balance is: " << balance << endl;
 					computerGameScore++;
-					diceGame = false;
+					break;
 
 				}
 			}
@@ -151,48 +167,44 @@ int main()
 
 			char rematch;
 
-			cout << "-----------------------------------" << endl;
+			if (!invalidInput) {
+				cout << "-----------------------------------" << endl;
 
-			cout << "The game score is now: Your score: " << humanGameScore << " and computer score: " << computerGameScore << endl;
+				cout << "The game score is now: Your score: " << humanGameScore << " and computer score: " << computerGameScore << endl;
 
-			cout << "Do you wanna play again (Y)?" << endl; cin >> rematch;
+				//If i put totalwinnings here it will add/remove the bet and even if i say totalwinnings +=/-= bet it will give it x2 because i put balance += (bet * 2)
+				cout << "The total winnings right now is: " << totalWinnings << endl;
 
-			if (rematch == 'Y' || rematch == 'y') {
-				wish2Continue = true;
+				cout << "Do you wanna play again (Y)?" << endl; cin >> rematch;
+
+				if (rematch == 'Y' || rematch == 'y') {
+					wish2Continue = true;
+				}
+				else {
+					cout << "This is your earnings for today: " << totalWinnings << " Kr " << endl;
+					betting = false;
+					break;
+				}
+
 			}
-			else {
-				wish2Continue = false;
-				Betting = false;
-			}
 
 
-
+			//IF the player want to play again but has less the lowest bet, the user will be questioned to add more to balance
 			if (balance < 100) {
-				cout << balance << "You don't have enough money to play again. \n Do you wanna add money? Yes(Y) or No(N)" << endl;
+				cout << "Your balance is: " << balance << "\nYou don't have enough money to play again.\nDo you wanna add money? Yes(Y) or No(N)" << endl;
 				cin >> addYesNo;
 
 				if (addYesNo == 'Y' || addYesNo == 'y') {
+					//This takes the player out of the game loop back to betting loop. So he/she can insert more money into the game.
 					wish2Continue = false;
-
-					if (balance > 5001 || balance < 0) {
-						cout << "Invalid amount";
-						cout << "Type in your wishing Balance again. (Max 5000): "; cin >> balance;
-					}
 				}
 				else {
+					betting = false;
 					wish2Continue = false;
-					Betting = false;
 				}
 
 			}
 
-
-			//cout << endl << "Type in R if wanna close";
-			//cin >> removeWhile;
-
-			//if (removeWhile == 'R') {
-			//	wish2Continue = false;
-			//}
 		}
 	}
 }
